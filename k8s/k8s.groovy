40ea5d2ef4771b0b8e4417a2c32144319923ff93
ipcrm <matthew.cadorette@lacework.net>
2022-04-04 14:00:18 -0400

    adding split k8s job

--- k8s/k8s.groovy
+++ k8s/k8s.groovy
@@ -1,13 +1,7 @@
 pipelineJob('k8s infastructure') {
   definition {
     cpsScm {
-      scm {
-        git {
-          remote {
-            url('https://github.com/lacework-dev/detc-resources.git','ipcrm/jenkins')
-          }
-        }
-      }
+      git('https://github.com/lacework-dev/detc-resources.git','ipcrm/jenkins')
     }
     scriptPath('jenkinsfiles/k8s/Jenkinsfile')
   }
