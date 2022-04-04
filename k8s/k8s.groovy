e40234fb46d571db40f987211f9a133791e7b087
ipcrm <matthew.cadorette@lacework.net>
2022-04-04 15:06:52 -0400

    disable build tags

--- k8s/k8s.groovy
+++ k8s/k8s.groovy
@@ -2,7 +2,7 @@ pipelineJob('k8s infastructure') {
   definition {
     cpsScm {
       scm {
-        git('https://github.com/lacework-dev/detc-resources.git','ipcrm/jenkins')
+        git('https://github.com/lacework-dev/detc-resources.git','ipcrm/jenkins', {node -> node / 'extensions' << '' })
       }
       scriptPath('jenkinsfiles/k8s/Jenkinsfile')
     }
